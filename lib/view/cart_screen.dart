c28e60f7a97350316062ea6d47fcdf4180ddbc0c
khaled <kha103led@yahoo.com>
2022-06-12 20:40:09 +0200

    update

--- lib/view/cart_screen.dart
+++ lib/view/cart_screen.dart
@@ -12,12 +12,16 @@ class CartScreen extends StatelessWidget {
     "product name",
     "product name",
     "product name",
+    "product name",
+    "product name",
   ];
   List<int> prices = [
     100,
     300,
     400,
     120,
+    160,
+    160,
     160,
   ];
   List<String> images = <String>[
@@ -26,119 +30,127 @@ class CartScreen extends StatelessWidget {
     "assets/images/fruits.png",
     "assets/images/shopping-cart.png",
     "assets/images/fruits.png",
+    "assets/images/fruits.png",
+    "assets/images/fruits.png",
   ];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 10),
-        child: ListView.separated(
-          physics: const BouncingScrollPhysics(),
-          itemBuilder: (context, index) {
-            return SizedBox(
-              height: MediaQuery.of(context).size.height * .2,
-              child: Row(
-                children: [
-                  SizedBox(
-                      height: MediaQuery.of(context).size.height * .14,
-                      width: MediaQuery.of(context).size.width * .3,
-                      child: Image.asset(
-                        images[index].toString(),
-                        fit: BoxFit.fill,
-                      )),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+      child: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: ListView.separated(
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    height: MediaQuery.of(context).size.height * .2,
+                    child: Row(
                       children: [
-                        const SizedBox(height: 8.0),
-                        Text(
-                          names[index].toString(),
-                          style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        const SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              prices[index].toString(),
-                              style: TextStyle(
-                                  color: Theme.of(context).primaryColor,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            const SizedBox(width: 3),
-                            const Text(
-                              "EGP",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.w400),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                                width: 30,
-                                height: 35,
-                                decoration: BoxDecoration(
-                                  color: primaryColor.withOpacity(.2),
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: const Icon(
-                                  Icons.add,
-                                  size :30,
-                                  color: Colors.black,
-                                )),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            const CustomText(
-                                alignment: Alignment.center,
-                                text: "1",
-                                fontSize: 20),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Container(
-                              width: 30,
-                              height: 35,
-                              decoration: BoxDecoration(
-                                color: primaryColor.withOpacity(.2),
-                                borderRadius: BorderRadius.circular(8),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * .14,
+                            width: MediaQuery.of(context).size.width * .3,
+                            child: Image.asset(
+                              images[index].toString(),
+                              fit: BoxFit.fill,
+                            )),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: 8.0),
+                              Text(
+                                names[index].toString(),
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w500),
                               ),
-                              child: SizedBox(
-                                height: 5,
-                                width: 5,
-                                child: Image.asset(
-                                  "assets/images/minus.png",
-                                  fit: BoxFit.fill,
-                                ),
+                              const SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    prices[index].toString(),
+                                    style: TextStyle(
+                                        color: Theme.of(context).primaryColor,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(width: 3),
+                                  const Text(
+                                    "EGP",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
+                              const SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                      width: 30,
+                                      height: 35,
+                                      decoration: BoxDecoration(
+                                        color: primaryColor.withOpacity(.2),
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                      child: const Icon(
+                                        Icons.add,
+                                        size :30,
+                                        color: Colors.black,
+                                      )),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  const CustomText(
+                                      alignment: Alignment.center,
+                                      text: "1",
+                                      fontSize: 20),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Container(
+                                    width: 30,
+                                    height: 35,
+                                    decoration: BoxDecoration(
+                                      color: primaryColor.withOpacity(.2),
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    child: SizedBox(
+                                      height: 5,
+                                      width: 5,
+                                      child: Image.asset(
+                                        "assets/images/minus.png",
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                  );
+                },
+                itemCount: names.length,
+                separatorBuilder: (BuildContext context, int index) {
+                  return const SizedBox(
+                    height: 10,
+                  );
+                },
               ),
-            );
-          },
-          itemCount: names.length,
-          separatorBuilder: (BuildContext context, int index) {
-            return const SizedBox(
-              height: 10,
-            );
-          },
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
