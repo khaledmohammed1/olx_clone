ddc2527b35eeb77ea2f4890e350f3b2c097c497e
khaled <kha103led@yahoo.com>
2022-06-26 08:42:14 +0200

    continue develop

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class LocationScreen extends StatelessWidget {
   const LocationScreen({Key? key}) : super(key: key);
 
@@ -6,9 +7,30 @@ class LocationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(children: [
-        Image.asset("assets/images/location.jpg"),
-      ],),
+      body: Column(
+        children: [
+          Image.asset("assets/images/location.jpg"),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "where you want \n buy/sell products",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            "to enjoy all that we have to offer you \nwe need to knew where to look for them",
+            textAlign: TextAlign.center,
+            style: TextStyle( fontSize: 12),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
     );
   }
 }
